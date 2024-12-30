94ff605229cf27e365e980cf3979a10511a9d41b
skeleton1231 <814010079@qq.com>
2024-12-30 08:43:57 +0800

    fix

--- user_data/strategies/sample_strategy_enhancned.py
+++ user_data/strategies/sample_strategy_enhancned.py
@@ -28,7 +28,7 @@ class SampleStrategyEnhanced(IStrategy):
     # ROI defined in the config as minimal_roi
     timeframe = "5m"
     process_only_new_candles = True
-    # use_custom_stoploss = False
+    use_custom_stoploss = False
 
     # Declare additional timeframes
     additional_timeframes = ["1h"]
